0a6bd31fb21b1fc9f7fbffd3bcfb892f14cfb5b7
Ben Lam <bklam@andrew.cmu.edu>
2014-04-30 01:20:16 -0400

    Started adding in logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ActiveRecord::Base
     role.downcase.to_sym == authorized_role
   end
 
+  # login by username
+  def self.authenticate(username, password)
+    find_by_username(username).try(:authenticate, password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,14 @@ ChessCamp::Application.routes.draw do
   get 'home/contact', to: 'home#contact', as: :contact
   get 'home/privacy', to: 'home#privacy', as: :privacy
 
+  # Authentication routes
+  resources :sessions
+  resources :users
+  get 'users/new', to: 'users#new', as: :signup
+  get 'user/edit', to: 'users#edit', as: :edit_current_user
+  get 'login', to: 'sessions#new', as: :login
+  get 'logout', to: 'sessions#new', as: :logout
+
   # set the root url
   root to: 'home#index'
 
