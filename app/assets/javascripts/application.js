a1bf01cdb22700128ed67f3afa393af02dc7bb9d
Ben Lam <bklam@andrew.cmu.edu>
2014-05-06 23:34:28 -0400

    calendar doesn't work.....

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,8 @@
 //= require jquery.ui.all
 //= require jquery_nested_form
 //= require foundation
-//= require_tree .
+//= require fullcalendar.min.js
+//= require fullcalendar.js
 
 $(function(){ $(document).foundation(); });
 
@@ -24,5 +25,37 @@ $(function() {
   $(".datepicker").datepicker({
     format: 'mm/dd/YYYY'
   });
+});
+
+// Calendar code
+$('#calendar').fullCalendar({
+
+    eventSources: [
+
+        // your event source
+        {
+            events: [ // put the array in the `events` property
+                {
+                    title  : 'event1',
+                    start  : '2010-01-01'
+                },
+                {
+                    title  : 'event2',
+                    start  : '2010-01-05',
+                    end    : '2010-01-07'
+                },
+                {
+                    title  : 'event3',
+                    start  : '2010-01-09 12:30:00',
+                }
+            ],
+            color: 'black',     // an option!
+            textColor: 'yellow' // an option!
+        }
+
+        // any other event sources...
+
+    ]
+
 });
 
