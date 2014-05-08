2e9bb4f48d90965af2c1d7b78645988c1461ae80
Ben Lam <bklam@andrew.cmu.edu>
2014-05-07 20:18:41 -0400

    Added in instructor dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if logged_in?
+      @instructor = Instructor.find(current_user.instructor_id)
+      @personal_upcoming_camps = @instructor.camps
+    end
   end
 
   def about
